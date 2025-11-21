faa8477cb3bff7214e930a8e72150c994ad98e5e
Marcos <marcosreyespavon@gmail.com>
2025-11-21 13:46:37 +0100

    TRABAJO TERMINADO final

--- src/Barco.java
+++ src/Barco.java
@@ -1,24 +1,56 @@
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class Barco {
 
     private final List<Pasajero> pasajeros = new ArrayList<>();
 
     public Barco() {
-        generarPasajeros();
+        generarPasajerosAleatorios();
     }
 
-    private void generarPasajeros() {
+    private void generarPasajerosAleatorios() {
         int id = 1;
+        Random rand = new Random();
 
-        for (int i = 0; i < 100; i++) pasajeros.add(new Pasajero(id++, 1));
-        for (int i = 0; i < 150; i++) pasajeros.add(new Pasajero(id++, 2));
-        for (int i = 0; i < 80; i++) pasajeros.add(new Pasajero(id++, 3));
-        for (int i = 0; i < 22; i++) pasajeros.add(new Pasajero(id++, 4));
+        // Contadores de cada categoría
+        int ninos = 100;
+        int adultos = 150;
+        int ancianos = 80;
+        int tripulacion = 22;
 
-        pasajeros.sort(Comparator.comparingInt(Pasajero::getPrioridad));
+        while (ninos + adultos + ancianos + tripulacion > 0) {
+            int categoria = rand.nextInt(4) + 1; // elige 1 a 4 aleatoriamente
+
+            switch (categoria) {
+                case 1:
+                    if (ninos > 0) {
+                        pasajeros.add(new Pasajero(id++, 1));
+                        ninos--;
+                    }
+                    break;
+                case 2:
+                    if (adultos > 0) {
+                        pasajeros.add(new Pasajero(id++, 2));
+                        adultos--;
+                    }
+                    break;
+                case 3:
+                    if (ancianos > 0) {
+                        pasajeros.add(new Pasajero(id++, 3));
+                        ancianos--;
+                    }
+                    break;
+                case 4:
+                    if (tripulacion > 0) {
+                        pasajeros.add(new Pasajero(id++, 4));
+                        tripulacion--;
+                    }
+                    break;
+            }
+        }
+
+        // Mezclar toda la lista para que el orden sea aleatorio
+        Collections.shuffle(pasajeros);
     }
 
     public List<Pasajero> getPasajeros() {
